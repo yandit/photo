58e0fc631e4d500d53e30f0b4a9421d08c40852c
yudit <wayanyuditya@gmail.com>
2024-01-16 23:00:31 +0800

    text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,10 +61,9 @@
             <div class="row">
                 <div class="left-text col-lg-8 col-md-12 col-sm-12 col-xs-12"
                     data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
-                    <h1>Simple App that we <em>CREATE</em></h1>
-                    <p>Lava <a href="#">HTML landing page</a> template is provided by <a href="#">TemplateMo</a>.
-                        You can modify and use it for your commercial websites for free of charge. This template is last updated on 29 Oct 2019.</p> 
-                    <a href="#about" class="main-button-slider">KNOW US BETTER</a>
+                    <h1>Pick your <em>Memories</em> and bring them to life.</h1>
+                    <p>Select your best memorable memories, and let us do the rest.</p> 
+                    <a href="{{ route('upload.index') }}" class="main-button-slider">Choose your memories</a>
                 </div>
             </div>
         </div>
